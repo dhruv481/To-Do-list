c903e18fadec29ba62c6809dc4ac3f84fc78806d
dhruv481 <dguptadg01@gmail.com>
2022-11-12 23:58:31 +0530

    adding media function

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 //import './Button.css';
 
 const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -13,6 +14,10 @@ const Button = styled.button`
 
   &:focus {
     outline: none;
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
   }
 
   &:hover,
